af11b35a8a3971e2c578d2722a86fc8090b5ddc7
BaileyRMH <BMadiganHennessy2024@elam.co.uk>
2024-10-22 09:55:26 +0100

    Moving platforms

---
+++ Assets/Scripts/PlatformSlidePatch.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSlidePatch : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            collision.gameObject.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            collision.gameObject.transform.SetParent(null);
+        }
+    }
+
+}
